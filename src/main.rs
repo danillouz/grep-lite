8ea5083bd0284230e4f27413ed02788cf58a2691
Daniël Illouz <daniel.illouz@me.com>
2020-01-28 22:44:16 +0100

    Search for a pattern with some hardcoded text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+/// Search text for a pattern.
+/// Based on this grep specification: https://pubs.opengroup.org/onlinepubs/9699919799/utilities/grep.html
 fn main() {
-    println!("Hello, world!");
+    let quote = "It wasn’t always so clear, but the Rust programming language
+is fundamentally about empowerment: no matter what kind of code you are
+writing now, Rust empowers you to reach farther, to program with confidence
+in a wider variety of domains than you did before.";
+    let query = "Rust";
+
+    for (i, line) in quote.lines().enumerate() {
+        if line.contains(query) {
+            let line_num = i + 1;
+            println!("{}: {}", line_num, line);
+        }
+    }
 }
