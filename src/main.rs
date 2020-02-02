b93f2091d2cfefa0b920a111252d9e32e1e66fa1
Daniël Illouz <daniel.illouz@me.com>
2020-02-02 17:37:30 +0100

    Use clap to accept the pattern as a command line arg

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
+extern crate clap;
 extern crate regex;
 
+use clap::{App, Arg};
 use regex::Regex;
 
 /// Search text for a pattern.
 /// Based on this grep specification: https://pubs.opengroup.org/onlinepubs/9699919799/utilities/grep.html
 fn main() {
+    let args = App::new("grep-lite")
+        .version("0.1.0")
+        .about("A lightweight version of the grep utility.")
+        .arg(
+            Arg::with_name("pattern")
+                .help("The pattern to search for.")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
+
     let quote = "It wasn’t always so clear, but the Rust programming language
 is fundamentally about empowerment: no matter what kind of code you are
 writing now, Rust empowers you to reach farther, to program with confidence
 in a wider variety of domains than you did before.";
-    let re = Regex::new("Rust").unwrap();
+
     let ctx_lines = 2;
     let mut tags: Vec<usize> = Vec::new();
     let mut ctx: Vec<Vec<(usize, String)>> = Vec::new();
