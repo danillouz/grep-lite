91568f76a598365a775f3dfe4c8c1814b9488abc
Daniël Illouz <daniel.illouz@me.com>
2020-02-02 18:13:46 +0100

    Allow to read from STDIN when no file matches

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,19 @@ use clap::{App, Arg};
 use regex::Regex;
 
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
+
+fn read_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for _line in reader.lines() {
+        let line = _line.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => (),
+        }
+    }
+}
 
 /// Search text for a pattern.
 /// Based on this grep specification: https://pubs.opengroup.org/onlinepubs/9699919799/utilities/grep.html
@@ -21,27 +32,24 @@ fn main() {
                 .required(true),
         )
         .arg(
-            Arg::with_name("file")
-                .help("The file to search through.")
+            Arg::with_name("input")
+                .help("The file, or text from STDIN to search through.")
                 .takes_value(true)
-                .required(true),
+                .required(false),
         )
         .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let file = args.value_of("file").unwrap();
-    let f = File::open(file).unwrap();
-    let reader = BufReader::new(f);
-
-    for _line in reader.lines() {
-        let line = _line.unwrap();
-        match re.find(&line) {
-            Some(_) => {
-                println!("{}", line);
-            }
-            None => (),
-        }
+    let input = args.value_of("input").unwrap_or("-");
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        read_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        read_lines(reader, re);
     }
 }
